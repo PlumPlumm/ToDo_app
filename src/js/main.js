e45376a1b93136b77e9e8b1463bba84a906cae9b
Plum <test123@mail.ru>
2022-06-18 22:13:28 +0500

    layout: creating a form for creating task names

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,30 @@ const btnCreatTask = document.querySelector('.sidebar__createTask');
 const sidebarList = document.querySelector('.sidebar__taskList');
 
 
+let totalTask = 0;
 //Создание задачи
 function createTask(){
-    let taskBlock = document.createElement('li');
-    taskBlock.className = 'sidebar__taskItem';
-    taskBlock.innerHTML = `
-        <p class="sidebar__text">хуй хуй хуй хуй</p>
-        <input class="sidebar__del" onclick="delTask" type="image" src="./images/trash.svg" alt="Удалить задачу">`;
 
-    sidebarList.prepend(taskBlock);
+    let createTaskName = () => {
+        let nameForm = document.createElement('div');
+        nameForm.className = 'sidebar__nameform';
+        nameForm.innerHTML 
+    }
+
+    formTaskNam();
+
+    // let taskBlock = document.createElement('li');
+    // totalTask++;
+    // taskBlock.className = 'sidebar__taskItem';
+    // taskBlock.id = ` task_${totalTask}`;
+    // taskBlock.innerHTML = `
+    //     <p class="sidebar__text">хуй хуй хуй хуй</p>
+    //     <input class="sidebar__del" onclick="delTask" type="image" src="./images/trash.svg" alt="Удалить задачу">`;
+
+    
+    // sidebarList.prepend(taskBlock);
+}
+
+function delTask(){
+
 }
