d1d629298c04cfd28f4da9cee07d0440ded025e9
Plum <test123@mail.ru>
2022-06-18 22:49:12 +0500

    js: calling the creating of a name by clicking on the create task button

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,12 @@
 const btnCreatTask = document.querySelector('.sidebar__createTask');
 const sidebarList = document.querySelector('.sidebar__taskList');
-
+const nameTask = document.querySelector('.nameform__input')
 
 let totalTask = 0;
 //Создание задачи
 function createTask(){
 
-    let createTaskName = () => {
-        let nameForm = document.createElement('div');
-        nameForm.className = 'sidebar__nameform';
-        nameForm.innerHTML 
-    }
-
-    formTaskNam();
-
+    createTaskName();
     // let taskBlock = document.createElement('li');
     // totalTask++;
     // taskBlock.className = 'sidebar__taskItem';
@@ -26,6 +19,21 @@ function createTask(){
     // sidebarList.prepend(taskBlock);
 }
 
-function delTask(){
 
-}
+let createTaskName = () => {
+    let nameForm = document.createElement('div');
+    nameForm.className = 'nameform';
+    nameForm.innerHTML = `<div class="nameform__container">
+        <h6 class="nameform__title">Введитe название задачи</h6>
+    <input type="text" class="nameform__input" placeholder="Название задачи" autofocus>
+    <div class="nameform__btn_wrapper">
+        <button class="nameform__btn">Сохранить</button>
+        <button class="nameform__btn">Отмена</button>
+    </div>
+     </div>`;
+
+     
+
+    document.body.prepend(nameForm);
+};
+
