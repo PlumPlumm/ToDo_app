dba1d635470df299d5fc1451d18714ffad88e533
Plum <test123@mail.ru>
2022-06-21 22:20:27 +0500

    js: btn save and close in nameform

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,48 @@
 const btnCreatTask = document.querySelector('.sidebar__createTask');
 const sidebarList = document.querySelector('.sidebar__taskList');
-const nameTask = document.querySelector('.nameform__input')
+
 
 let totalTask = 0;
 //Создание задачи
 function createTask(){
+//Вывод окна naveform
+    let nameForm = document.createElement('div');
+    nameForm.className = 'nameform';
+    nameForm.innerHTML = `<div class="nameform__container">
+        <h6 class="nameform__title">Введитe название задачи</h6>
+    <input type="text" class="nameform__input" placeholder="Название задачи" autofocus>
+    <div class="nameform__btn_wrapper">
+        <button class="nameform__btn" id='btnSave'>Сохранить</button>
+        <button class="nameform__btn" id='btnClose'>Отмена</button>
+    </div>
+    </div>`;
+    document.body.prepend(nameForm);
+    
+    const nameForm_btnSave = document.getElementById('btnSave');
+    const nameForm_btnClose = document.getElementById('btnClose');
+    const nameForm_input = document.querySelector('.nameform__input');
 
-    createTaskName();
+    
+    nameForm_btnSave.onclick = () => {
+        let nameTask = nameForm_input.value;
+        let taskBlock = document.createElement('li');
+        totalTask++;
+        taskBlock.className = 'sidebar__taskItem';
+        taskBlock.id = ` task_${totalTask}`;
+        taskBlock.innerHTML = `<p class="sidebar__text">${nameTask}</p> 
+                                <input class="sidebar__del" onclick="delTask" type="image" src="./images/trash.svg" alt="Удалить задачу">`;
+        
+        sidebarList.prepend(taskBlock);
+        nameForm.remove();
+    }
+
+    nameForm_btnClose.onclick = () => {
+        nameForm.remove();
+    }
+
+
+    
+
     // let taskBlock = document.createElement('li');
     // totalTask++;
     // taskBlock.className = 'sidebar__taskItem';
@@ -20,20 +56,4 @@ function createTask(){
 }
 
 
-let createTaskName = () => {
-    let nameForm = document.createElement('div');
-    nameForm.className = 'nameform';
-    nameForm.innerHTML = `<div class="nameform__container">
-        <h6 class="nameform__title">Введитe название задачи</h6>
-    <input type="text" class="nameform__input" placeholder="Название задачи" autofocus>
-    <div class="nameform__btn_wrapper">
-        <button class="nameform__btn">Сохранить</button>
-        <button class="nameform__btn">Отмена</button>
-    </div>
-     </div>`;
-
-     
-
-    document.body.prepend(nameForm);
-};
 
